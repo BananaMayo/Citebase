e37bd9f9f3e940103b0daa35f148622a5a86b91f
BananaMayo <niklas.nygard01@gmail.com>
2022-11-24 16:04:44 +0200

    book citation setup

---
+++ src/new_citation.py
@@ -0,0 +1,11 @@
+def book_citation():
+    title = input('title: ')
+    year = input('year: ')
+    author = input('author: ')
+    publisher = input('publisher: ')
+
+    print(title, year, author, publisher)
+
+
+if __name__ == "__main__":
+    book_citation()
