5329c213c7b3a26cff8172c4f5213969405905b2
Lotta Tanner <lotta.tanner@helsinki.fi>
2022-11-24 16:16:56 +0200

    citation services

---
+++ src/entities/book.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, title, author, year, publisher):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.publisher = publisher
