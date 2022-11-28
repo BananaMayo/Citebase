7a8b0e915069b58d0b8d21bb947119190a9e239c
Antti Hartikka <antti.hartikka@helsinki.fi>
2022-11-28 16:01:55 +0200

    front refactor

--- src/front_end/new_citation.py
+++ src/front_end/new_citation.py
@@ -1,19 +1,10 @@
 from services.citation_service import citation_services
 
+
 def book_citation():
     title = input('Title: ')
     author = input('Author: ')
     year = input('Year: ')
     publisher = input('Publisher: ')
 
-    #print(title, author, year, publisher)
-
     citation_services.create_book(title, author, year, publisher)
-
-def show_books():
-    titles = citation_services.show_books()
-    print('\n'.join(titles))
-
-
-if __name__ == "__main__":
-    book_citation()
