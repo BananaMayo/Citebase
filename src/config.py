de373f78b6ec76f87c6dad50b715a4b8a6bae501
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2022-11-28 16:38:18 +0200

    tests

--- src/config.py
+++ src/config.py
@@ -11,4 +11,6 @@ dirname = os.path.dirname(__file__)
 #DB_FILENAME = os.getenv("DB_FILENAME") or "database.sqlite"
 
 DB_FILENAME = "database.sqlite"
+DB_FILENAME_TEST="test-database.sqlite"
 DB_FILE_PATH = os.path.join(dirname, '..', 'data', DB_FILENAME)
+DB_FILE_PATH_TEST=os.path.join(dirname, '..', 'data', DB_FILENAME_TEST)
