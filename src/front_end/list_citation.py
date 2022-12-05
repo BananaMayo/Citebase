30d5eea98368245bede4ec1ced2675e82b486ecf
Lotta Tanner <lotta.tanner@helsinki.fi>
2022-12-05 16:25:26 +0200

    empty list notification

--- src/front_end/list_citation.py
+++ src/front_end/list_citation.py
@@ -3,4 +3,6 @@ from services.citation_service import citation_services
 
 def list_book_titles(io):
     titles = citation_services.show_books()
+    if len(titles) == 0:
+        titles = ["No book titles!"]
     io.print('\n'.join(titles))
