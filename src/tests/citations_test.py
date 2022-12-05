2f501555966b8fa0cdcaf801b61890ca2004fef9
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2022-12-05 16:39:28 +0200

    more test

--- src/tests/citations_test.py
+++ src/tests/citations_test.py
@@ -11,9 +11,34 @@ class TestCitations(unittest.TestCase):
         self.testrepo=CitationRepository(get_test_database_connection())
         self.testservices=citation_services
         self.book_1=Book("Harry Potter","Rowling",2000,"Otava")
+        self.book_2=Book("Taru sormusten herrasta","Tolkien",2022,"Otava")
+        self.book_3=Book("Muumipeikko ja pyrstötähti","Tove Jansson",2000,"Otava")
         initialize_test_database()
 
 
     def test_create_book_citation(self):
         test= self.testrepo.create_book(self.book_1)
         self.assertEqual(test, "Citation added successfully")
+
+    def test_show_books(self):
+        self.testrepo.create_book(self.book_1)
+        self.testrepo.create_book(self.book_2)
+        self.testrepo.create_book(self.book_3)
+        books=self.testrepo.show_books()
+        self.assertEqual(books, [self.book_1.title,self.book_2.title,self.book_3.title])
+
+    def test_delete_book(self):
+        self.testrepo.create_book(self.book_1)
+        self.testrepo.create_book(self.book_2)
+        test=self.testrepo.delete_book(self.book_1.title)
+        test_list=self.testrepo.show_books()
+        self.assertEqual(test, "Book removed successfully")
+        self.assertEqual(len(test_list), 1)
+
+    def test_delete_all_books(self):
+        self.testrepo.create_book(self.book_1)
+        self.testrepo.create_book(self.book_2)
+        test=self.testrepo.delete_all_books()
+        test_list=self.testrepo.show_books()
+        self.assertEqual(test, "All books removed successfully")
+        self.assertEqual(len(test_list), 0)
