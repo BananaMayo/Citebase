e751d1a0ad1c90a236f2fbc10f265d27c4ec6120
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2022-12-05 16:19:20 +0200

    update test

--- src/tests/citations_test.py
+++ src/tests/citations_test.py
@@ -11,20 +11,9 @@ class TestCitations(unittest.TestCase):
         self.testrepo=CitationRepository(get_test_database_connection())
         self.testservices=citation_services
         self.book_1=Book("Harry Potter","Rowling",2000,"Otava")
-        self.book_2=Book("Taru sormusten herrasta","Tolkien",2022,"Otava")
-        self.book_3=Book("Muumipeikko ja pyrstötähti","Tove Jansson",2000,"Otava")
         initialize_test_database()
 
 
     def test_create_book_citation(self):
         test= self.testrepo.create_book(self.book_1)
         self.assertEqual(test, "Citation added successfully")
-
-    
-    def test_show_books(self):
-        self.testrepo.create_book(self.book_1)
-        self.testrepo.create_book(self.book_2)
-        self.testrepo.create_book(self.book_3)
-        books=self.testrepo.show_books()
-        print(books)
-        self.assertEqual(books, ["Harry Potter","Taru sormusten herrasta","Muumipeikko ja pyrstötähti"])
