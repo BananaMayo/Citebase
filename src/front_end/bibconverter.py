30dd6b23b27291090a69508fd5f9ad855fb951db
BananaMayo <niklas.nygard01@gmail.com>
2022-12-07 13:30:49 +0200

    front component

--- src/front_end/bibconverter.py
+++ src/front_end/bibconverter.py
@@ -1,8 +1,8 @@
 #Bibtex:in luonti 
 from services.citation_service import citation_services
 
-def create_bib_file(io):
-    result = citation_services.create_bib()
+def create_bib_file(io, services=citation_services):
+    result = services.create_bib()
     io.print(result)
 
 
