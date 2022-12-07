f749dbd9d2f8e73fb072b7de827d2fdacd224890
Lotta Tanner <lotta.tanner@helsinki.fi>
2022-12-07 13:07:59 +0200

    whitespace

--- src/AppLibrary.py
+++ src/AppLibrary.py
@@ -13,13 +13,11 @@ class AppLibrary:
         initialize_test_database()
         self._io = StubIO()
         self._app = App(self._io, self.test_services)
-    
     def input(self, value):
         self._io.add_input(value)
 
     def output_should_contain(self, value):
         outputs = self._io.outputs
-
         if not value in outputs:
             raise AssertionError(
                 f"Output \"{value}\" is not in {str(outputs)}"
--- src/app.py
+++ src/app.py
@@ -4,7 +4,6 @@ from front_end.delete_book import delete_book
 from front_end.delete_all import delete_all
 from services.citation_service import citation_services
 
-
 class App:
     def __init__(self, io, services=citation_services):
         self.io = io
@@ -13,18 +12,13 @@ class App:
     def run(self):
         while True:
             user_input = self.io.input('next command (new book, list books, exit, delete book, delete all):')
-
             if user_input == 'exit':
                 break
-
             if user_input == 'new book':
                 book_citation(self.io, self.services)
-
             if user_input == "list books":
                 list_book_titles(self.io, self.services)
-            
             if user_input == "delete book":
                 delete_book(self.io, self.services)
-            
             if user_input == "delete all":
                 delete_all(self.io, self.services)
