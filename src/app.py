5a541fd1b48ec5a68c47f64c3b82750135cad23b
laurelcrelia <laurel.matt@helsinki.fi>
2022-12-12 15:38:45 +0200

    komentorivillä automaattinen clear -toiminto

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from front_end.delete_book import delete_book
 from front_end.delete_all import delete_all
 from front_end.bibconverter import create_bib_file
 from services.citation_service import citation_services
+import os
 
 
 class App:
@@ -16,14 +17,20 @@ class App:
             user_input = self.io.input('next command (new book, list books, exit, delete book, delete all, bib):')
 
             if user_input == 'exit':
+                os.system("clear")
                 break
             if user_input == 'new book':
+                os.system("clear")
                 book_citation(self.io, self.services)
             if user_input == "list books":
+                os.system("clear")
                 list_book_titles(self.io, self.services)
             if user_input == "delete book":
+                os.system("clear")
                 delete_book(self.io, self.services)
             if user_input == "delete all":
+                os.system("clear")
                 delete_all(self.io, self.services)
             if user_input == "bib":
+                os.system("clear")
                 create_bib_file(self.io, self.services)
