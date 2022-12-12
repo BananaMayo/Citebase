06d5c914d76f2cc2d41d52fb657692a6e48ed52e
Antti Hartikka <antti.hartikka@helsinki.fi>
2022-12-12 16:13:50 +0200

    import funcion v1
    Co-authored-by: Niklas Nygård <niklas.nygard@helsinki.fi>

--- src/import.py
+++ src/import.py
@@ -1,19 +1,22 @@
 from entities.book import Book
 
-f = open("biblio.bib","r")
-lines = f.readlines()
-print(lines)
+def import_from_file(path):
+    f = open(path, "r")
+    lines = f.readlines()
 
-books = []
+    books = []
 
-for i in range(0, len(lines)):
-    if lines[i].startswith('@book'):
-        title = lines[i+1][10:][:-4]
-        author = lines[i+2][11:][:-4]
-        year = lines[i+3][9:][:-4]
-        publisher = lines[i+4][14:][:-3]
-        book = Book(title, author, year, publisher)
-        books.append(book)
-        print(title, author, year, publisher)
+    for i, line in enumerate(lines):
+        if line.startswith('@book'):
+            title = lines[i + 1][10:][:-4]
+            author = lines[i + 2][11:][:-4]
+            year = lines[i + 3][9:][:-4]
+            publisher = lines[i + 4][14:][:-3]
+            book = Book(title, author, year, publisher)
+            books.append(book)
 
-print(books)
+    return books
+
+
+if __name__ == "__main__":
+    print(import_from_file(input("path: ")))
