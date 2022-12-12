fb4a7df33f7a8707c6d4a91e1b63f5946feedabe
Lotta Tanner <lotta.tanner@helsinki.fi>
2022-12-12 15:46:48 +0200

    import order fix

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
+import os
 from front_end.new_citation import book_citation
 from front_end.list_citation import list_book_titles
 from front_end.delete_book import delete_book
 from front_end.delete_all import delete_all
 from front_end.bibconverter import create_bib_file
 from services.citation_service import citation_services
-import os
 
 
 class App:
