4f8f2b59de371e21131972a8a8d5f908334680d9
laurelcrelia <laurel.matt@helsinki.fi>
2022-12-13 16:59:21 +0200

    clearer view of commands

--- src/front_end/list_commands.py
+++ src/front_end/list_commands.py
@@ -1,5 +1,14 @@
 
 def list_command_names(io):
-    names = ["\u001b[0mexit", "\u001b[32mnew book", "\u001b[35mlist books", "\u001b[33mdelete book", "\u001b[31mdelete all", "\u001b[36mexport", "\u001b[34mimport"]
-    io.print('\n'.join(names))
-    io.print(" ")
+    commands = [("\u001b[00mexit", "\u001b[00mExit app"), 
+                ("\u001b[32mnew book", "\u001b[00mAdd new book"),
+                ("\u001b[35mlist books", "\u001b[00mList all books"),
+                ("\u001b[33mdelete book", "\u001b[00mDelete single book"),
+                ("\u001b[31mdelete all", "\u001b[00mDelete all books"),
+                ("\u001b[36mexport", "\u001b[00mExport all books to bib file"),
+                ("\u001b[34mimport", "\u001b[00mImport all books from bib file")
+                ]
+
+    for command, explanation in commands:
+        io.print(f"{command:20} {explanation}")
+
