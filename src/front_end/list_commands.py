e450f272cf0cba8010e2aad9ddc677a85e5bad57
laurelcrelia <laurel.matt@helsinki.fi>
2022-12-13 19:31:18 +0200

    more clear commands view

--- src/front_end/list_commands.py
+++ src/front_end/list_commands.py
@@ -1,14 +1,13 @@
 
 def list_command_names(io):
-    commands = [("\u001b[00mexit", "\u001b[00mExit app"), 
-                ("\u001b[32mnew book", "\u001b[00mAdd new book"),
-                ("\u001b[35mlist books", "\u001b[00mList all books"),
-                ("\u001b[33mdelete book", "\u001b[00mDelete single book"),
-                ("\u001b[31mdelete all", "\u001b[00mDelete all books"),
-                ("\u001b[36mexport", "\u001b[00mExport all books to bib file"),
-                ("\u001b[34mimport", "\u001b[00mImport all books from bib file")
+    commands = [("\u001b[1;38;5;064mexit", "\u001b[0;000mExit app"), 
+                ("\u001b[1;38;5;084mnew book", "\u001b[0;000mAdd new book"),
+                ("\u001b[1;38;5;221mlist books", "\u001b[0;000mList all books"),
+                ("\u001b[1;38;5;208mdelete book", "\u001b[0;000mDelete single book"),
+                ("\u001b[1;38;5;196mdelete all", "\u001b[0;000mDelete all books"),
+                ("\u001b[1;38;5;039mexport", "\u001b[0;000mExport all books to bib file"),
+                ("\u001b[1;38;5;092mimport", "\u001b[0;000mImport all books from bib file")
                 ]
-
     for command, explanation in commands:
-        io.print(f"{command:20} {explanation}")
+        io.print(f"{command:26} {explanation}")
 
