aea6cbf8584ad4ed0c530ea6ee225444ece3c09a
Antti Hartikka <antti.hartikka@helsinki.fi>
2022-12-13 14:46:04 +0200

    book entity test
    Co-authored-by: Niklas Nygård <niklas.nygard@helsinki.fi>

---
+++ src/tests/book_entity_test.py
@@ -0,0 +1,7 @@
+import unittest
+from entities.book import Book
+
+class BookTest(unittest.TestCase):
+    def test_book_repr(self):
+        book = Book("How to Book", "Who Is", "1900", "Indie Books Inc.")
+        self.assertEqual(book.__repr__(), "How to Book         Who Is              1900                Indie Books Inc.    ")
