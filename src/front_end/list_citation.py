f87c507148980f90e387a34d5d8885adb9b01a6e
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2022-12-13 20:24:46 +0200

    frontend updates for citation listing view

--- src/front_end/list_citation.py
+++ src/front_end/list_citation.py
@@ -3,8 +3,7 @@ from services.citation_service import citation_services
 def list_book_titles(io, services=citation_services):
     books = services.show_books()
     if len(books) == 0:
-        books = ["No book titles!"]
+        books = ["\u001b[1;38;5;221mNo book titles!"]
     for book in books:
         io.print(book)
-    io.print(" ")
     
