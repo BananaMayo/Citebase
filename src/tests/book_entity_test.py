f09a4b2949acd063d2f343056fb3d69a7877858a
Antti Hartikka <antti.hartikka@helsinki.fi>
2022-12-15 13:38:35 +0200

    fixed broken test

--- src/tests/book_entity_test.py
+++ src/tests/book_entity_test.py
@@ -4,4 +4,4 @@ from entities.book import Book
 class BookTest(unittest.TestCase):
     def test_book_repr(self):
         book = Book("How to Book", "Who Is", "1900", "Indie Books Inc.")
-        self.assertEqual(book.__repr__(), "How to Book         Who Is              1900                Indie Books Inc.    ")
+        self.assertEqual(book.__repr__(), "How to Book                       Who Is              1900  Indie Books Inc.    ")
